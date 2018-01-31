d43f0070cac6934c88ac86a99c0af50929f9e854
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2018-02-01 00:39:36 +0600

    update pgfn

--- pgfn.js
+++ pgfn.js
@@ -1,21 +1,21 @@
 const { Client } = require('pg');
 
-module.exports = function pgfn(sql) {
-  return function (req, res, next) {
-    client = new Client({
-      user: 'sowyer',
-      host: 'localhost',
-      database: 'pgdb',
-      password: 'sowyer',
-      port: 5432,
-    })
-    client.connect()
-    client.query(sql, (err, result) => {
-      if (err) {
-        return err.stack;
-      }
-      res.send(result.rows);
-      client.end();
-    });
-  }
+module.exports = function pgfn(sql, params, cb) {
+  client = new Client({
+    user: 'sowyer',
+    host: 'localhost',
+    database: 'pgdb',
+    password: 'sowyer',
+    port: 5432,
+  })
+  client.connect()
+  client.query(sql, params, (err, result) => {
+    if (err) {
+      throw Error(err.stack);
+    }
+    if (cb) {
+      cb(result)
+    }
+    client.end();
+  });
 }
